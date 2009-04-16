6ef2a0181f95ab5e0721ae7b3863f9fbc37e06e7
Charles Loomis <loomis@lal.in2p3.fr>
2009-04-16 19:37:29 +0000

    add code to recursively process object templates when writing dependency information

--- panc/src/org/quattor/pan/tasks/WriteDepTask.java
+++ panc/src/org/quattor/pan/tasks/WriteDepTask.java
@@ -23,9 +23,14 @@ package org.quattor.pan.tasks;
 import static org.quattor.pan.utils.MessageUtils.MSG_CANNOT_CREATE_OUTPUT_DIRECTORY;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.PrintStream;
 import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
+import java.util.Stack;
 import java.util.concurrent.Callable;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -82,44 +87,129 @@ public class WriteDepTask extends Task<TaskResult> {
 
 			Valid2Cache v2cache = compiler.getValid2Cache();
 
-			// Now actually retrieve the other object's root, waiting if the
-			// result isn't yet available.
-			Valid2Result result = (Valid2Result) v2cache
-					.waitForResult(objectName);
-			long timestamp = result.timestamp;
-			Map<String, Template> dependencies = result.getDependencies();
+			// This list contains the names of all of the object templates that
+			// have already been processed. This is done to prevent reprocessing
+			// of object templates and infinite loops from circular
+			// dependencies.
+			List<String> processed = new LinkedList<String>();
+
+			// This list contains all of the unprocessed object templates. The
+			// initial template to process is added to start the process.
+			Stack<String> unprocessed = new Stack<String>();
+			unprocessed.push(objectName);
+
+			// Use URI instances to operate on the output directory and the
+			// object name. The object name can be namespaced, so this extra
+			// processing is needed.
+			URI odir = outputDirectory.toURI();
+			URI oname = new URI(objectName);
+			URI resolvedAbsoluteURI = odir.resolve(oname);
+			String resolvedAbsolutePath = resolvedAbsoluteURI
+					.getSchemeSpecificPart()
+					+ ".xml.dep";
+			File absolutePath = new File(resolvedAbsolutePath);
+
+			// Extract the parent directory and ensure that it exists. If
+			// the creation fails, ignore it. The error will be caught
+			// below.
+			File parent = absolutePath.getParentFile();
+			if (!parent.exists() && !parent.mkdirs()) {
+				throw new SystemException(MessageUtils.format(
+						MSG_CANNOT_CREATE_OUTPUT_DIRECTORY, parent
+								.getAbsolutePath()), parent);
+			}
+
+			// This will contain the timestamp of the file. This is a Long to
+			// allow the initial value to be null. We will take the timestamp of
+			// the first object template processed, which should be the correct
+			// one.
+			Long timestamp = null;
+
+			// Open the output file for the dependency information.
+			PrintStream ps = new PrintStream(absolutePath);
 
 			// Mark the beginning of writing dependency file.
 			taskLogger.log(Level.FINER, "START_DEPFILE", objectName);
 
-			if (dependencies != null) {
+			// Loop until there are no more unprocessed object templates.
+			while (!unprocessed.empty()) {
+				String objectToProcess = unprocessed.pop();
 
-				// Use URI instances to operate on the output directory and the
-				// object name. The object name can be namespaced, so this extra
-				// processing is needed.
-				URI odir = outputDirectory.toURI();
-				URI oname = new URI(objectName);
-				URI resolvedAbsoluteURI = odir.resolve(oname);
-				String resolvedAbsolutePath = resolvedAbsoluteURI
-						.getSchemeSpecificPart()
-						+ ".xml.dep";
-				File absolutePath = new File(resolvedAbsolutePath);
+				// Only do something if the object template hasn't already been
+				// processed.
+				if (!processed.contains(objectToProcess)) {
+					processed.add(objectToProcess);
 
-				// Extract the parent directory and ensure that it exists. If
-				// the creation fails, ignore it. The error will be caught
-				// below.
-				File parent = absolutePath.getParentFile();
-				if (!parent.exists() && !parent.mkdirs()) {
-					throw new SystemException(MessageUtils.format(
-							MSG_CANNOT_CREATE_OUTPUT_DIRECTORY, parent
-									.getAbsolutePath()), parent);
+					// Get the result from the cache, waiting if necessary.
+					Valid2Result result = (Valid2Result) v2cache
+							.waitForResult(objectToProcess);
+
+					// Extract the timestamp, if necessary.
+					if (timestamp == null) {
+						timestamp = Long.valueOf(result.timestamp);
+					}
+
+					// Write out the dependencies. Add all of the object
+					// templates encountered during the processing to the
+					// unprocessed list.
+					unprocessed.addAll(writeDependencies(ps, result
+							.getDependencies()));
+
 				}
 
-				PrintStream ps = new PrintStream(absolutePath);
+			}
+
+			ps.close();
+
+			// Mark the end of writing dependency file.
+			taskLogger.log(Level.FINER, "END_DEPFILE", objectName);
+
+			// Make sure the modification time corresponds to the given
+			// timestamp.
+			if (!absolutePath.setLastModified(timestamp)) {
+				// Probably a warning should be emitted here, but currently
+				// there are no facilities for warnings in the pan compiler
+				// yet.
+			}
+
+			return new TaskResult(TaskResult.ResultType.DEP);
+		}
+
+		/**
+		 * This method walks through the given dependencies and writes them to
+		 * the given stream.
+		 * 
+		 * @param ps
+		 *            PrintStream to write the dependencies to
+		 * @param dependencies
+		 *            list of dependencies
+		 * 
+		 * @return list of object templates encountered during processing
+		 * 
+		 * @throws URISyntaxException
+		 * @throws FileNotFoundException
+		 */
+		private List<String> writeDependencies(PrintStream ps,
+				Map<String, Template> dependencies) throws URISyntaxException,
+				FileNotFoundException {
+
+			// List to hold names of object templates encountered during
+			// processing.
+			List<String> objectTemplates = new LinkedList<String>();
+
+			if (dependencies != null) {
+
 				for (Map.Entry<String, Template> entry : dependencies
 						.entrySet()) {
 					String tname = entry.getKey();
-					String tfile = entry.getValue().source.toString();
+					Template template = entry.getValue();
+					String tfile = template.source.toString();
+
+					// If the template is an object template, then save the name
+					// for further processing by the parent.
+					if (template.type == Template.TemplateType.OBJECT) {
+						objectTemplates.add(template.name);
+					}
 
 					// Must strip off the template name from the file name.
 					String fsname = tname.replace('/', File.separatorChar)
@@ -135,22 +225,10 @@ public class WriteDepTask extends Task<TaskResult> {
 
 					ps.printf("\"%s\" \"%s\"%n", tname, tfile);
 				}
-				ps.close();
-
-				// Make sure the modification time corresponds to the given
-				// timestamp.
-				if (!absolutePath.setLastModified(timestamp)) {
-					// Probably a warning should be emitted here, but currently
-					// there are no facilities for warnings in the pan compiler
-					// yet.
-				}
 
 			}
 
-			// Mark the end of writing dependency file.
-			taskLogger.log(Level.FINER, "END_DEPFILE", objectName);
-
-			return new TaskResult(TaskResult.ResultType.DEP);
+			return objectTemplates;
 		}
 	}
 

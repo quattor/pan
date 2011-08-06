fac52e631074fcaf22dae6e1094eace4b5f92dfd
Charles Loomis <loomis@lal.in2p3.fr>
2011-08-06 11:05:02 +0200

    add support for backspace and form feed escape sequences (SF Bug#3386906)

--- panc/src/main/java/org/quattor/pan/parser/PanParserUtils.java
+++ panc/src/main/java/org/quattor/pan/parser/PanParserUtils.java
@@ -30,203 +30,209 @@ import org.quattor.pan.template.SourceRange;
 
 public class PanParserUtils {
 
-	static void addSpecialTokens(SimpleNode node, Token token) {
-		for (Token currentToken = token; currentToken.specialToken != null; currentToken = currentToken.specialToken) {
-			node.addSpecialToken(currentToken.specialToken);
-		}
-	}
+    static void addSpecialTokens(SimpleNode node, Token token) {
+        for (Token currentToken = token; currentToken.specialToken != null; currentToken = currentToken.specialToken) {
+            node.addSpecialToken(currentToken.specialToken);
+        }
+    }
 
-	static SourceRange sourceRangeFromTokens(Token start, Token end) {
-		return new SourceRange(start.beginLine, start.beginColumn, end.endLine,
-				end.endColumn);
-	}
+    static SourceRange sourceRangeFromTokens(Token start, Token end) {
+        return new SourceRange(start.beginLine, start.beginColumn, end.endLine,
+                end.endColumn);
+    }
 
-	/**
-	 * A utility to replace HEREDOC operations with the associated
-	 * StringProperty operations. This must be done at the end of the template
-	 * processing to be sure that all heredoc strings have been captured.
-	 * 
-	 * @param tnode
-	 * @param strings
-	 */
-	static void replaceHeredocStrings(ASTTemplate tnode,
-			List<StringProperty> strings) {
+    /**
+     * A utility to replace HEREDOC operations with the associated
+     * StringProperty operations. This must be done at the end of the template
+     * processing to be sure that all heredoc strings have been captured.
+     * 
+     * @param tnode
+     * @param strings
+     */
+    static void replaceHeredocStrings(ASTTemplate tnode,
+            List<StringProperty> strings) {
 
-		// Only something to do if there were heredoc strings defined.
-		if (strings.size() > 0) {
-			processHeredocStrings(tnode, strings);
-		}
-	}
+        // Only something to do if there were heredoc strings defined.
+        if (strings.size() > 0) {
+            processHeredocStrings(tnode, strings);
+        }
+    }
 
-	/**
-	 * Descend through the AST and replace individual ASTOperation (HEREDOC)
-	 * nodes with the associated strings.
-	 * 
-	 * @param node
-	 * @param strings
-	 */
-	static private void processHeredocStrings(SimpleNode node,
-			List<StringProperty> strings) {
+    /**
+     * Descend through the AST and replace individual ASTOperation (HEREDOC)
+     * nodes with the associated strings.
+     * 
+     * @param node
+     * @param strings
+     */
+    static private void processHeredocStrings(SimpleNode node,
+            List<StringProperty> strings) {
 
-		// If there are any children, then process them in post-traversal order.
-		int count = node.jjtGetNumChildren();
-		if (count > 0) {
-			for (int i = 0; i < count; i++) {
-				SimpleNode n = (SimpleNode) node.jjtGetChild(i);
+        // If there are any children, then process them in post-traversal order.
+        int count = node.jjtGetNumChildren();
+        if (count > 0) {
+            for (int i = 0; i < count; i++) {
+                SimpleNode n = (SimpleNode) node.jjtGetChild(i);
 
-				// If this is a heredoc literal, then actually update the node
-				// with the string.
-				if (n instanceof ASTOperation) {
-					ASTOperation astop = (ASTOperation) n;
-					if (astop.getOperationType() == OperationType.HEREDOC) {
+                // If this is a heredoc literal, then actually update the node
+                // with the string.
+                if (n instanceof ASTOperation) {
+                    ASTOperation astop = (ASTOperation) n;
+                    if (astop.getOperationType() == OperationType.HEREDOC) {
 
-						Operation op = astop.getOperation();
-						if (op instanceof LongProperty) {
-							LongProperty lprop = (LongProperty) op;
-							int index = lprop.getValue().intValue();
-							StringProperty sprop = strings.get(index);
-							astop.setOperationType(OperationType.LITERAL);
-							astop.setOperation(sprop);
-						} else {
-							throw new RuntimeException(
-									"HEREDOC operation doesn't contain LongProperty");
-						}
-					}
-				}
+                        Operation op = astop.getOperation();
+                        if (op instanceof LongProperty) {
+                            LongProperty lprop = (LongProperty) op;
+                            int index = lprop.getValue().intValue();
+                            StringProperty sprop = strings.get(index);
+                            astop.setOperationType(OperationType.LITERAL);
+                            astop.setOperation(sprop);
+                        } else {
+                            throw new RuntimeException(
+                                    "HEREDOC operation doesn't contain LongProperty");
+                        }
+                    }
+                }
 
-				// Alway process any children.
-				processHeredocStrings(n, strings);
-			}
-		}
-	}
+                // Alway process any children.
+                processHeredocStrings(n, strings);
+            }
+        }
+    }
 
-	/**
-	 * This takes a single-quoted string as identified by the parser and
-	 * processes it into a standard java string. It removes the surrounding
-	 * quotes and substitues a single apostrophe or doubled apostrophes.
-	 * 
-	 * Note: This method modifies the given StringBuilder directly. No copies of
-	 * the data are made.
-	 * 
-	 * @param sb
-	 *            StringBuilder containing the single-quoted string
-	 */
-	static void processSingleQuotedString(StringBuilder sb) {
+    /**
+     * This takes a single-quoted string as identified by the parser and
+     * processes it into a standard java string. It removes the surrounding
+     * quotes and substitues a single apostrophe or doubled apostrophes.
+     * 
+     * Note: This method modifies the given StringBuilder directly. No copies of
+     * the data are made.
+     * 
+     * @param sb
+     *            StringBuilder containing the single-quoted string
+     */
+    static void processSingleQuotedString(StringBuilder sb) {
 
-		// Sanity checking. Make sure input string has at least two characters
-		// and that the first and last are single quotes.
-		assert (sb.length() >= 2);
-		assert (sb.charAt(0) == '\'' && sb.charAt(sb.length() - 1) == '\'');
+        // Sanity checking. Make sure input string has at least two characters
+        // and that the first and last are single quotes.
+        assert (sb.length() >= 2);
+        assert (sb.charAt(0) == '\'' && sb.charAt(sb.length() - 1) == '\'');
 
-		// Remove the starting and ending quotes.
-		sb.deleteCharAt(sb.length() - 1);
-		sb.deleteCharAt(0);
+        // Remove the starting and ending quotes.
+        sb.deleteCharAt(sb.length() - 1);
+        sb.deleteCharAt(0);
 
-		// Loop through the string and replace any doubled apostrophes with
-		// a single apostrophe. (Really just delete the second apostrophe.)
-		int i = sb.indexOf("''", 0) + 1;
-		while (i > 0) {
-			sb.deleteCharAt(i);
-			i = sb.indexOf("''", i) + 1;
-		}
-	}
+        // Loop through the string and replace any doubled apostrophes with
+        // a single apostrophe. (Really just delete the second apostrophe.)
+        int i = sb.indexOf("''", 0) + 1;
+        while (i > 0) {
+            sb.deleteCharAt(i);
+            i = sb.indexOf("''", i) + 1;
+        }
+    }
 
-	/**
-	 * This method takes a double-quoted string as identified by the parser and
-	 * creates a standard java string from it.
-	 * 
-	 * Note: The StringBuilder is modified directly. No copies of the data are
-	 * made.
-	 * 
-	 * @param sb
-	 *            StringBuilder which contains the double-quoted string to
-	 *            convert
-	 * @param sourceRange
-	 *            source location for double-quoted string
-	 */
-	static void processDoubleQuotedString(StringBuilder sb,
-			SourceRange sourceRange) throws ParseException {
+    /**
+     * This method takes a double-quoted string as identified by the parser and
+     * creates a standard java string from it.
+     * 
+     * Note: The StringBuilder is modified directly. No copies of the data are
+     * made.
+     * 
+     * @param sb
+     *            StringBuilder which contains the double-quoted string to
+     *            convert
+     * @param sourceRange
+     *            source location for double-quoted string
+     */
+    static void processDoubleQuotedString(StringBuilder sb,
+            SourceRange sourceRange) throws ParseException {
 
-		// Sanity checking. Make sure input string has at least two characters
-		// and that the first and last are double quotes.
-		assert (sb.length() >= 2);
-		assert (sb.charAt(0) == '"' && sb.charAt(sb.length() - 1) == '"');
+        // Sanity checking. Make sure input string has at least two characters
+        // and that the first and last are double quotes.
+        assert (sb.length() >= 2);
+        assert (sb.charAt(0) == '"' && sb.charAt(sb.length() - 1) == '"');
 
-		// Remove the starting and ending quotes.
-		sb.deleteCharAt(sb.length() - 1);
-		sb.deleteCharAt(0);
+        // Remove the starting and ending quotes.
+        sb.deleteCharAt(sb.length() - 1);
+        sb.deleteCharAt(0);
 
-		// Scan through the string looking for backslashes.
-		int i = sb.indexOf("\\", 0);
-		while (i >= 0) {
+        // Scan through the string looking for backslashes.
+        int i = sb.indexOf("\\", 0);
+        while (i >= 0) {
 
-			if (i + 1 >= sb.length()) {
-				ParseException pe = new ParseException(
-						"double-quoted string contains incomplete escape sequence");
-				pe.sourceRange = sourceRange;
-				throw pe;
-			}
+            if (i + 1 >= sb.length()) {
+                ParseException pe = new ParseException(
+                        "double-quoted string contains incomplete escape sequence");
+                pe.sourceRange = sourceRange;
+                throw pe;
+            }
 
-			switch (sb.charAt(i + 1)) {
-			case 'n':
-				sb.replace(i, i + 2, "\n");
-				break;
-			case 't':
-				sb.replace(i, i + 2, "\t");
-				break;
-			case 'r':
-				sb.replace(i, i + 2, "\r");
-				break;
-			case '\\':
-				sb.replace(i, i + 2, "\\");
-				break;
-			case '"':
-				sb.replace(i, i + 2, "\"");
-				break;
-			case 'x':
-				try {
-					int codepoint = Integer.parseInt(
-							sb.substring(i + 2, i + 4), 16);
-					char[] chars = Character.toChars(codepoint);
-					sb.replace(i, i + 4, Character.toString(chars[0]));
-				} catch (StringIndexOutOfBoundsException se) {
-					ParseException pe = new ParseException(
-							"double-quoted string contains incomplete hex escape sequence");
-					pe.sourceRange = sourceRange;
-					throw pe;
-				} catch (NumberFormatException e) {
-					ParseException pe = new ParseException(
-							"double-quoted string contains illegal hex replacement ("
-									+ sb.substring(i + 2, i + 4) + ")");
-					pe.sourceRange = sourceRange;
-					throw pe;
-				}
-				break;
-			case '\r':
-				// Fall through.
-			case '\n':
-				int count = ((i + 2 < sb.length()) && "\r\n".equals(sb
-						.substring(i + 1, i + 3))) ? 3 : 2;
-				sb.delete(i, i + count);
+            switch (sb.charAt(i + 1)) {
+            case 'n':
+                sb.replace(i, i + 2, "\n");
+                break;
+            case 't':
+                sb.replace(i, i + 2, "\t");
+                break;
+            case 'r':
+                sb.replace(i, i + 2, "\r");
+                break;
+            case '\\':
+                sb.replace(i, i + 2, "\\");
+                break;
+            case '"':
+                sb.replace(i, i + 2, "\"");
+                break;
+            case 'b':
+                sb.replace(i, i + 2, "\b");
+                break;
+            case 'f':
+                sb.replace(i, i + 2, "\f");
+                break;
+            case 'x':
+                try {
+                    int codepoint = Integer.parseInt(
+                            sb.substring(i + 2, i + 4), 16);
+                    char[] chars = Character.toChars(codepoint);
+                    sb.replace(i, i + 4, Character.toString(chars[0]));
+                } catch (StringIndexOutOfBoundsException se) {
+                    ParseException pe = new ParseException(
+                            "double-quoted string contains incomplete hex escape sequence");
+                    pe.sourceRange = sourceRange;
+                    throw pe;
+                } catch (NumberFormatException e) {
+                    ParseException pe = new ParseException(
+                            "double-quoted string contains illegal hex replacement ("
+                                    + sb.substring(i + 2, i + 4) + ")");
+                    pe.sourceRange = sourceRange;
+                    throw pe;
+                }
+                break;
+            case '\r':
+                // Fall through.
+            case '\n':
+                int count = ((i + 2 < sb.length()) && "\r\n".equals(sb
+                        .substring(i + 1, i + 3))) ? 3 : 2;
+                sb.delete(i, i + count);
 
-				// Because this replaces the entire sequence with nothing, the
-				// counter must decremented. If it isn't, then the first
-				// character after the replacement will never be checked for a
-				// escape sequence. (See SF bug #2533401.)
-				i--;
+                // Because this replaces the entire sequence with nothing, the
+                // counter must decremented. If it isn't, then the first
+                // character after the replacement will never be checked for a
+                // escape sequence. (See SF bug #2533401.)
+                i--;
 
-				break;
-			default:
-				ParseException pe = new ParseException(
-						"double-quoted string contains an illegal escape sequence (\\"
-								+ sb.charAt(i + 1) + ")");
-				pe.sourceRange = sourceRange;
-				throw pe;
-			}
+                break;
+            default:
+                ParseException pe = new ParseException(
+                        "double-quoted string contains an illegal escape sequence (\\"
+                                + sb.charAt(i + 1) + ")");
+                pe.sourceRange = sourceRange;
+                throw pe;
+            }
 
-			i++;
-			i = sb.indexOf("\\", i);
-		}
-	}
+            i++;
+            i = sb.indexOf("\\", i);
+        }
+    }
 
 }
--- panc/src/test/java/org/quattor/pan/parser/PanParserUtilsTest.java
+++ panc/src/test/java/org/quattor/pan/parser/PanParserUtilsTest.java
@@ -22,25 +22,56 @@ package org.quattor.pan.parser;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.junit.Test;
 
 public class PanParserUtilsTest {
 
-	@Test
-	public void testIgnoredEscapeSequence() {
+    @Test
+    public void testValidEscapeSequences() {
 
-		String result = "a\nb";
+        Map<String, String> testPairs = new HashMap<String, String>();
 
-		// Because of an error in scanning the string, escape replacements after
-		// a line continuation were ignored. (See SF bug #2533401.)
-		String[] inputs = { "\"a\\\n\\nb\"", "\"a\\\r\\nb\"", "\"a\\\r\n\\nb\"" };
+        testPairs.put("\"\\t\"", "\t");
+        testPairs.put("\"\\b\"", "\b");
+        testPairs.put("\"\\n\"", "\n");
+        testPairs.put("\"\\r\"", "\r");
+        testPairs.put("\"\\f\"", "\f");
+        testPairs.put("\"\\\"\"", "\"");
+        testPairs.put("\"\\\\\"", "\\");
+        testPairs.put("\"\\x12\"", "\u0012");
 
-		for (String input : inputs) {
-			StringBuilder sb = new StringBuilder(input);
-			PanParserUtils.processDoubleQuotedString(sb, null);
-			assertEquals(result, sb.toString());
-		}
+        for (Map.Entry<String, String> entry : testPairs.entrySet()) {
 
-	}
+            StringBuilder input = new StringBuilder(entry.getKey());
+            PanParserUtils.processDoubleQuotedString(input, null);
+            String result = input.toString();
+
+            String correct = entry.getValue();
+
+            assertEquals(correct, result);
+
+        }
+
+    }
+
+    @Test
+    public void testIgnoredEscapeSequence() {
+
+        String result = "a\nb";
+
+        // Because of an error in scanning the string, escape replacements after
+        // a line continuation were ignored. (See SF bug #2533401.)
+        String[] inputs = { "\"a\\\n\\nb\"", "\"a\\\r\\nb\"", "\"a\\\r\n\\nb\"" };
+
+        for (String input : inputs) {
+            StringBuilder sb = new StringBuilder(input);
+            PanParserUtils.processDoubleQuotedString(sb, null);
+            assertEquals(result, sb.toString());
+        }
+
+    }
 
 }

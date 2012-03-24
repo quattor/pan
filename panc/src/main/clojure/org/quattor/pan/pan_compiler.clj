7c215dd4e3d67f140428257734234563d3469952
Charles Loomis <cal@sixsq.com>
2012-03-24 22:21:26 +0100

    skeletons of new bash scripts for commands

--- panc/src/main/clojure/org/quattor/pan/pan_compiler.clj
+++ panc/src/main/clojure/org/quattor/pan/pan_compiler.clj
@@ -1,6 +1,59 @@
 (ns org.quattor.pan.pan-compiler
   (:gen-class)
-  (:use clojure.tools.cli))
+  (:use clojure.tools.cli)
+  (:import (java.io File)
+           (org.quattor.pan.output PanFormatter)
+           (org.quattor.pan CompilerOptions CompilerResults)))
+
+(defn create-absolute-file [name]
+  (.getAbsoluteFile (File. name)))
+
+(defn current-directory []
+  (create-absolute-file ""))
+
+(defn get-compiler-options [options]
+  (let [debugIncludePatterns []
+        debugExcludePatterns []
+        xmlWriteEnabled true
+        depWriteEnabled true
+        iterationLimit 10000
+        callDepthLimit 10
+        formatter (PanFormatter/getInstance)
+        outputDirectory (current-directory)
+        sessionDirectory nil
+        includeDirectories [(current-directory)]
+        nthread 0
+        gzipOutput false
+        deprecationLevel -1
+        forceBuild false
+        annotationDirectory nil
+        annotationBaseDirectory nil
+        failOnWarn false
+        rootElement nil]
+    (CompilerOptions. debugIncludePatterns 
+                      debugExcludePatterns 
+                      xmlWriteEnabled 
+                      depWriteEnabled 
+                      iterationLimit 
+                      callDepthLimit 
+                      formatter 
+                      outputDirectory 
+                      sessionDirectory 
+                      includeDirectories 
+                      nthread 
+                      gzipOutput
+                      deprecationLevel 
+                      forceBuild 
+                      annotationDirectory
+                      annotationBaseDirectory
+                      failOnWarn 
+                      rootElement)))
+
+(defn build-profiles [options files] 
+  (let [compiler-options (get-compiler-options options)
+        pan-sources (map #(File. %) files)]
+    (org.quattor.pan.Compiler/run compiler-options nil pan-sources)))
+
 
 (defn -main [& args]
   (let [[options files banner]
@@ -26,5 +79,8 @@
     (when (:help options)
       (println banner)
       (System/exit 0))
-    (println options)
-    (println files)))
+    (let [results (build-profiles options files)]
+      (if-let [errors (.formatErrors results)]
+        (println errors)
+        "")
+      (println (.formatStats results)))))

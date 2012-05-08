007a0c6cb07a185a3cc9d9f7e45f41400f487437
Charles Loomis <cal@sixsq.com>
2012-05-08 14:14:02 +0200

    ensure that options name matches between script and compiler driver file

--- panc/src/main/clojure/org/quattor/pan/pan_compiler.clj
+++ panc/src/main/clojure/org/quattor/pan/pan_compiler.clj
@@ -56,31 +56,36 @@
 
 
 (defn -main [& args]
-  (let [[options files banner]
-        (cli args
-             ["--debug" "enable all debugging" :default false :flag true]
-             ["--debug-ns-include" "ns regex for debugging"]
-             ["--debug-ns-exclude" "ns regex to exclude debugging"]
-             ["--initial-data" "set root element (must be nlist)"]
-             ["--include-path" "template lookup path"]
-             ["--session-dir" "session directory"]
-             ["--output-dir" "output directory" ]
-             ["--formats" "generated output formats" :default "pan,dep"]
-             ["--compress" "compress output" :default false :flag true]
-             ["--jvm-options" "options for JVM"]
-             ["--max-iteration" "set max. no. of iterations" :default 10000]
-             ["--max-recursion" "set max. depth of recursion" :default 10]
-             ["--logging" "set logging types"]
-             ["--log-file" "specify log file"]
-             ["--warnings" "off, on, fatal" :default "on" ]
-             ["-v" "--verbose" "show statistics and progress" :default false :flag true]
-             ["-h" "--help" "print command help" :default false :flag true]
-             )]
-    (when (:help options)
-      (println banner)
-      (System/exit 0))
-    (let [results (build-profiles options files)]
-      (if-let [errors (.formatErrors results)]
-        (println errors)
-        "")
-      (println (.formatStats results)))))
+  (try
+    (let [[options files banner]
+          (cli args
+               ["--debug" "enable all debugging" :default false :flag true]
+               ["--debug-ns-include" "ns regex for debugging"]
+               ["--debug-ns-exclude" "ns regex to exclude debugging"]
+               ["--initial-data" "set root element (must be nlist)"]
+               ["--include-path" "template lookup path"]
+               ["--session-dir" "session directory"]
+               ["--output-dir" "output directory" ]
+               ["--formats" "generated output formats" :default "pan,dep"]
+               ["--compress" "compress output" :default false :flag true]
+               ["--java-opts" "options for JVM"]
+               ["--max-iteration" "set max. no. of iterations" :default 10000]
+               ["--max-recursion" "set max. depth of recursion" :default 10]
+               ["--logging" "set logging types"]
+               ["--log-file" "specify log file"]
+               ["--warnings" "off, on, fatal" :default "on" ]
+               ["-v" "--verbose" "show statistics and progress" :default false :flag true]
+               ["-h" "--help" "print command help" :default false :flag true]
+               )]
+      (when (:help options)
+        (println banner)
+        (System/exit 0))
+      (let [results (build-profiles options files)]
+        (if-let [errors (.formatErrors results)]
+          (println errors)
+          "")
+        (println (.formatStats results))))
+    (catch Exception e
+      (println (.getMessage e))
+      (System/exit 1))))
+  

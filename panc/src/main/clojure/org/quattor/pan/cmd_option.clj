263b2fdfacc2914ebe643572bdec3fd79a2a6e64
Charles Loomis <cal@sixsq.com>
2012-05-09 21:23:38 +0200

    add some more command line processing and tests

--- panc/src/main/clojure/org/quattor/pan/cmd_option.clj
+++ panc/src/main/clojure/org/quattor/pan/cmd_option.clj
@@ -1,10 +1,34 @@
 (ns org.quattor.pan.cmd-option
-  (:use [clojure.string :only [split]])
+  (:use [clojure.string :only [split blank?]])
   (:import (org.quattor.pan.output TxtFormatter 
                                    JsonFormatter 
                                    DotFormatter 
                                    PanFormatter 
                                    XmlDBFormatter)))
+(defn pattern-list
+  "Takes a list of regular expression patterns in a single
+   string separated by whitespace and returns a list of
+   regular expressions."
+  [s]
+  (let [patterns (or s "")]
+    (map re-pattern (filter (complement blank?) (split patterns #"\s+")))))
+
+(defn to-integer
+  "Converts string to integer.  If the string is not a valid
+   integer, then the method returns nil."
+  [s]
+  (try
+    (Integer/valueOf s)
+    (catch Exception e nil)))
+
+(defn positive-integer
+  [name value]
+  (if-let [i (to-integer value)]
+    (if (pos? i)
+      {name i}
+      (throw (Exception. (str "only postive values for " name "are allowed"))))
+    (throw (Exception. (str "invalid integer value (" value ") for " name)))))
+
 
 (defmulti process
   "Process a command line option given the name and
@@ -40,3 +64,28 @@
                  "dep" {:depWriteEnabled true}
                  (throw (Exception. (str "unknown formatter in formats: " formatter-name)))))
              formatter-names))))
+
+(defmethod process :compress
+  [name value]
+  (if value
+    {:gzip-output true}
+    {:gzip-output false}))
+
+(defmethod process :debug-exclude-patterns
+  [name value]
+  {name (pattern-list value)})
+
+(defmethod process :debug-include-patterns
+  [name value]
+  {name (pattern-list value)})
+
+(defmethod process :iteration-limit
+  [name value]
+  (positive-integer name value))
+
+(defmethod process :call-depth-limit
+  [name value]
+  (positive-integer name value))
+
+
+
--- panc/src/test/clojure/org/quattor/pan/cmd_option_test.clj
+++ panc/src/test/clojure/org/quattor/pan/cmd_option_test.clj
@@ -1,11 +1,27 @@
 (ns org.quattor.pan.cmd-option-test
   (:use clojure.test
+        [clojure.string :only [join]]
         org.quattor.pan.cmd-option)
   (:import (org.quattor.pan.output TxtFormatter 
                                    JsonFormatter 
                                    DotFormatter 
                                    PanFormatter 
-                                   XmlDBFormatter)))
+                                   XmlDBFormatter)
+           (java.util.regex Pattern)))
+
+(deftest test-to-integer
+  []
+  (is (= 0 (to-integer "0")))
+  (is (= 1001 (to-integer "1001")))
+  (is (= -1 (to-integer "-1")))
+  (is (nil? (to-integer "a"))))
+
+(deftest test-positive-integer
+  []
+  (is (= 1 (:name (positive-integer :name "1"))))
+  (is (= 1000 (:name (positive-integer :name "1000"))))
+  (is (thrown? Exception (positive-integer :name "-1")))
+  (is (thrown? Exception (positive-integer :name "a"))))
 
 (deftest test-default
   []
@@ -41,3 +57,57 @@
     (is (instance? XmlDBFormatter (:formatter xmldb))))
   (is (thrown? Exception (process :formats "unknown"))))
 
+(deftest test-compress
+  []
+  (is (:gzip-output (process :compress true)))
+  (is (not (:gzip-output (process :compress false)))))
+
+(deftest test-debug-exclude-patterns
+  []
+  (let [patterns [".*OK.*" ".*OK2.*"]
+        patterns-as-string (join " " patterns)
+        result (process :debug-exclude-patterns patterns-as-string)
+        regex (:debug-exclude-patterns result)]
+    (is (= 1 (count result)))
+    (is (= 2 (count regex)))
+    (is (every? (partial instance? Pattern) regex))
+    (let [[regex1 regex2] regex]
+      (is (re-matches regex1 "...OK..."))
+      (is (re-matches regex1 "xxxOKxxx"))
+      (is (not (re-matches regex1 "...BAD...")))
+      (is (re-matches regex2 "...OK2..."))
+      (is (re-matches regex2 "xxxOK2xxx"))
+      (is (not (re-matches regex2 "...BAD2..."))))))
+
+(deftest test-debug-include-patterns
+  []
+  (let [patterns [".*OK.*" ".*OK2.*"]
+        patterns-as-string (join " " patterns)
+        result (process :debug-include-patterns patterns-as-string)
+        regex (:debug-include-patterns result)]
+    (is (= 1 (count result)))
+    (is (= 2 (count regex)))
+    (is (every? (partial instance? Pattern) regex))
+    (let [[regex1 regex2] regex]
+      (is (re-matches regex1 "...OK..."))
+      (is (re-matches regex1 "xxxOKxxx"))
+      (is (not (re-matches regex1 "...BAD...")))
+      (is (re-matches regex2 "...OK2..."))
+      (is (re-matches regex2 "xxxOK2xxx"))
+      (is (not (re-matches regex2 "...BAD2..."))))))
+
+(deftest test-iteration-limit
+  []
+  (is (= 1 (:iteration-limit (process :iteration-limit "1"))))
+  (is (= 1000 (:iteration-limit (process :iteration-limit "1000"))))
+  (is (thrown? Exception (process :iteration-limit "-1")))
+  (is (thrown? Exception (process :iteration-limit "a"))))
+  
+(deftest test-call-depth-limit
+  []
+  (is (= 1 (:call-depth-limit (process :call-depth-limit "1"))))
+  (is (= 1000 (:call-depth-limit (process :call-depth-limit "1000"))))
+  (is (thrown? Exception (process :call-depth-limit "-1")))
+  (is (thrown? Exception (process :call-depth-limit "a"))))
+  
+  

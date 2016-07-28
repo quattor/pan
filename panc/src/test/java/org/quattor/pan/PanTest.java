3f918e00d5d153d3161061a8c1a272b754392313
Isaura <claeysisaura@gmail.com>
2016-07-28 15:29:21 +0200

    Fixed issues

--- panc/src/test/java/org/quattor/pan/PanTest.java
+++ panc/src/test/java/org/quattor/pan/PanTest.java
@@ -6,15 +6,18 @@ import java.util.*;
 /**
  * Created by iliclaey.
  *
- * When building the panc-project a panc-10.3-SNAPSHOT-fat-jar-with-tests.jar file is created.
+ * When building the panc-project a panc-MAJOR.MINOR-SNAPSHOT-fat-jar-with-tests.jar file is created.
  * This jar can be used to execute pan test files and should be used as follows:
- * `java -Dpanc.testdir="path" -Dpanc.tmpdir="path" -jar path/to/panc-10.3-SNAPSHOT-fat-jar-with-tests.jar`.
+ * `java -Dpanc.testdir="path" -Dpanc.tmpdir="path" -jar path/to/panc-MAJOR.MINOR-SNAPSHOT-fat-jar-with-tests.jar`.
  * The panc.testdir and panc.tmpdir properties need to be set, otherwise an error will be thrown.
  *
  * To be able to execute your tests, they need to conform to a specific directory structure.
  * The directory you pass to the panc.testdir property needs to contain the _Functionality_,  _Dependency_ or
  * _RootElement_ directories, depending on what type of tests you want to run. Each of these directories need to be
  * divided in subdirectories, which contain the test files (or more subdirectories).
+ *
+ * This structure and the implementation is already being used to test the panc unittests, where the testfiles are
+ * located in the pan repo (panc/src/test/pan).
  *
  * panc.testdir
  * |-- Dependency
@@ -26,13 +29,6 @@ import java.util.*;
  * |   |   |-- file1.pan
  * |   |   |-- file2.pan
  * |-- RootElement
- *
- * Dependency tests
- * ================
- * A simple example:
- *
- * # dep: simple
- * object template simple;
  *
  * Functionality tests
  * ===================
@@ -46,15 +42,22 @@ import java.util.*;
  *
  * # @expect="/profile/result=1"
  *
+ * Dependency tests
+ * ================
+ * A simple example:
+ *
+ * # dep: simple
+ * object template simple;
+ *
  * RootElement tests
  * =================
  * Specific functionality tests to test the root element in the tree.
  *
  *
  * To run the tests in a specific directory, additional properties can be passed on to java:
- *      - test_functionality: runs the tests in the _Functionality_ directory. Default value is true.
- *      - test_dependency: runs the tests in the _Dependency_ directory. Default value is false.
- *      - test_rootelement: runs the tests in the _RootElement_ directory. Default value is false.
+ *      - test_functionality: runs the tests in the _Functionality_ subdirectory of panc.testdir. Default value is true.
+ *      - test_dependency: runs the tests in the _Dependency_ subdirectory of panc.testdir. Default value is false.
+ *      - test_rootelement: runs the tests in the _RootElement_ subdirectory of panc.testdir. Default value is false.
  *
  * For example, when you only want to run the tests in the _Dependency_ directory you would use the following command:
  * `java -Dpanc.testdir="path" -Dpanc.tmpdir="path" -Dtest_functionality="false" -Dtest_dependency="true" -jar ...`.
@@ -75,26 +78,18 @@ public class PanTest {
         JavaCompilerTest jct = new JavaCompilerTest();
 
         String functionality = props.getProperty("test_functionality");
-        if (functionality != null) {
-            if (Boolean.parseBoolean(functionality)) {
-                jct.javaFunctionalTests();
-            }
-        } else {
+        if (functionality == null || Boolean.parseBoolean(functionality)) {
             jct.javaFunctionalTests();
         }
 
         String dependency = props.getProperty("test_dependency");
-        if (dependency != null) {
-            if (Boolean.parseBoolean(dependency)) {
-                jct.javaDependencyTests();
-            }
+        if (dependency != null && Boolean.parseBoolean(dependency)) {
+            jct.javaDependencyTests();
         }
 
         String rootElement = props.getProperty("test_rootelement");
-        if (rootElement != null) {
-            if (Boolean.parseBoolean(rootElement)) {
-                jct.javaRootElementTests();
-            }
+        if (rootElement != null && Boolean.parseBoolean(rootElement)) {
+            jct.javaRootElementTests();
         }
     }
 }

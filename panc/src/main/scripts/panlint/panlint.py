6332dc47693fd07d42e2ae9893b72599dd3e238e
James Adams <james.adams@stfc.ac.uk>
2016-12-15 11:59:03 +0000

    Check for whitespace after semicolons

--- panc/src/main/scripts/panlint/panlint.py
+++ panc/src/main/scripts/panlint/panlint.py
@@ -51,6 +51,7 @@ LINE_PATTERNS = {
     "Line is longer than %s characters" % LINE_LENGTH_LIMIT: re.compile(r'''^.{0,%s}(?P<error>.*?)$''' % LINE_LENGTH_LIMIT),
     "Commas should be followed by exactly one space": re.compile(r'(?P<error>,(?:\S|\s{2,}))'),
     "Whitespace before semicolon": re.compile(r'(?P<error>\s+;)'),
+    "Semicolons should be followed exactly one space or end-of-line": re.compile(r'(?P<error>;(?:\S|\s{2,}))'),
 }
 
 TAB_ARROW = u'\u2192'

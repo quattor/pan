2dd9c32ed33ac579a8289d6725336835e29fc9f8
James Adams <james.adams@stfc.ac.uk>
2018-11-26 10:48:45 +0000

    Check ids of line patterns rather than the message
    The message may change in future, but the test id should not.

--- panc/src/main/scripts/panlint/tests.py
+++ panc/src/main/scripts/panlint/tests.py
@@ -502,31 +502,32 @@ class TestPanlint(unittest.TestCase):
     def test_check_line_patterns(self):
         lines = [
             ('variable UNIVERSAL_TRUTH = 42;', []),
-            ('variable BAD = -1;', ['Global variables should be five or more characters']),
-            ('variable bad_long = ":-(";', ['Global variables should be uppercase']),
-            ('variable bad = "all lower";', ['Global variables should be uppercase',
-                                             'Global variables should be five or more characters']),
-            ('variable tricky_onE = "Uhoh";', ['Global variables should be uppercase']),
-            ('variable camelCase = "camels!";', ['Global variables should be uppercase']),
-            ('variable TitleCase ?= -3;', ['Global variables should be uppercase']),
-            ('variable NoSpacesHere?=True;', ['Global variables should be uppercase']),
-            ('error(format("Duplicate %s in foo", mp));', ['Redundant use of format within error or debug call']),
+            ('variable BAD = -1;', ['LP011']),
+            ('variable bad_long = ":-(";', ['LP010']),
+            ('variable bad = "all lower";', ['LP010', 'LP011']),
+            ('variable tricky_onE = "Uhoh";', ['LP010']),
+            ('variable camelCase = "camels!";', ['LP010']),
+            ('variable TitleCase ?= -3;', ['LP010']),
+            ('variable NoSpacesHere?=True;', ['LP010']),
+            ('error(format("Duplicate %s in foo", mp));', ['LP012']),
             ('error("is_asndate: invalid format for time");', []),
-            ('debug(format("%s: bar: %s", OBJECT, ARGV[0]));', ['Redundant use of format within error or debug call']),
+            ('debug(format("%s: bar: %s", OBJECT, ARGV[0]));', ['LP012']),
             ('debug("Foo" + bar + " has an unexpected format (should be a dict)");', []),
-            ('"/software/components/metaconfig/services/{/etc/example_service_with_long_name/conf.d/dropin_config.cfg}/contents/foo" = "bar";', ['Line is longer than 120 characters']),
+            ('"/software/components/metaconfig/services/{/etc/example_service_with_long_name/conf.d/dropin_config.cfg}/contents/foo" = "bar";', ['LP006']),
             ('bind "/software/components/metaconfig/services/{/etc/example_service_with_long_name/conf.d/dropin_config.cfg}/contents" = type_example_service;', []),
         ]
 
-        for text, messages in lines:
+        for text, ids in lines:
+            ids.sort()
             line = panlint.Line('patterns.pan', 0, text)
-            messages = set(messages)
             problems = panlint.check_line_patterns(line, [])
+            problem_ids = [problem.message.id for problem in problems]
+            problem_ids.sort()
+            self.assertEqual(problem_ids, ids)
 
             self.assertIsInstance(problems, list)
             for p in problems:
                 self.assertIsInstance(p, panlint.Problem)
-            self.assertEqual(set([p.message.text for p in problems]), messages)
 
     def test_check_line_paths(self):
         problems = panlint.check_line_paths(
